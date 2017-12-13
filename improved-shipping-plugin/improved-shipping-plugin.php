33c9788c7c1468bb3e1cd05455516ba957eac246
Jakob Johansson <jakobjohansson2@icloud.com>
2017-12-13 13:36:03 +0100

    refactor some variables into class properties

--- improved-shipping-plugin/improved-shipping-plugin.php
+++ improved-shipping-plugin/improved-shipping-plugin.php
@@ -6,6 +6,39 @@
 function improvedShippingInit() {
     if ( ! class_exists( 'ImprovedShipping' ) ) {
         class ImprovedShipping extends WC_Shipping_Method {
+            /**
+             * The Google API key.
+             *
+             * @var string
+             */
+            private $key;
+
+            /**
+             * The shop zip code.
+             *
+             * @var string
+             */
+            private $shopZipCode;
+
+            /**
+             * The customer zip code.
+             *
+             * @var string
+             */
+            private $customerZipCode;
+
+            /**
+             * The total weight of the cart.
+             *
+             * @var int
+             */
+            private $weight;
+
+            /**
+             * Create a new ImprovedShipping instance.
+             *
+             * @return void
+             */
             public function __construct() {
                 $this->id                 = 'improved_shipping';
                 $this->method_title       = __( 'Improved Shipping' );
@@ -18,13 +51,23 @@ function improvedShippingInit() {
                 $this->init();
             }
 
-            function init() {
+            /**
+             * Initialize the shipping plugin.
+             *
+             * @return void
+             */
+            public function init() {
                 $this->init_form_fields();
                 $this->init_settings();
 
                 add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
             }
 
+            /**
+             * Initialize the form fields.
+             *
+             * @return void
+             */
             public function init_form_fields() {
                 $this->form_fields =  [
                     'shopcode' => [
@@ -36,26 +79,32 @@ function improvedShippingInit() {
                 ];
             }
 
+            /**
+             * Calculate the shipping cost.
+             *
+             * @param  array  $package
+             * @return void
+             */
             public function calculate_shipping( $package = []) {
                 // First, let's calculate the weight of the cart.
                 global $woocommerce;
-                $totalWeight = $woocommerce->cart->cart_contents_weight;
+                $this->weight = $woocommerce->cart->cart_contents_weight;
 
                 // Then calculate the distance to the customer.
-                $customerCode = $package['destination']['postcode'];
-                $customerCode = substr_replace($customerCode, "%20", 3, 0);
+                $this->customerZipCode = $package['destination']['postcode'];
+                $this->customerZipCode = substr_replace($this->customerZipCode, "%20", 3, 0);
 
-                $shopCode = substr_replace($this->get_option('shopcode'), "%20", 3, 0);
+                $this->shopZipCode = substr_replace($this->get_option('shopcode'), "%20", 3, 0);
 
-                $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={$shopCode}&destinations={$customerCode}&region=SE&units=metric&key={$this->key}";
+                $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={$this->shopZipCode}&destinations={$this->customerZipCode}&region=SE&units=metric&key={$this->key}";
 
                 // Check for cache!
-                if (get_transient('regular' . $customerCode . $shopCode) !== false) {
-                    $distance = get_transient('regular' . $customerCode . $shopCode);
+                if (get_transient('regular' . $this->customerZipCode . $this->shopZipCode) !== false) {
+                    $distance = get_transient('regular' . $this->customerZipCode . $this->shopZipCode);
                 } else {
                     $distance = json_decode(wp_remote_retrieve_body(wp_remote_get($url)));
                     $distance = $distance->rows[0]->elements[0]->distance->value;
-                    set_transient('regular' . $customerCode . $shopCode, $distance);
+                    set_transient('regular' . $this->customerZipCode . $this->shopZipCode, $distance);
                 }
 
                 // Make distance into km.
@@ -70,21 +119,21 @@ function improvedShippingInit() {
                 $this->add_rate(array(
                     'id' => $this->id,
                     'label' => 'Normal shipping',
-                    'cost' => $this->getCost($totalWeight, $distance),
+                    'cost' => $this->getCost($this->weight, $distance),
                     'calc_tax' => 'per_item'
                 ));
 
                 // Check if bicycle shipping can be applied.
-                if ($totalWeight < 5 && $realDistance < 10) {
+                if ($this->weight < 5 && $realDistance < 10) {
                     $url .= '&mode=bicycling';
 
                     // Check for cache!
-                    if (get_transient('bicycle' . $customerCode . $shopCode) !== false) {
-                        $bikeDistance = get_transient('bicycle' . $customerCode . $shopCode);
+                    if (get_transient('bicycle' . $this->customerZipCode . $this->shopZipCode) !== false) {
+                        $bikeDistance = get_transient('bicycle' . $this->customerZipCode . $this->shopZipCode);
                     } else {
                         $bikeDistance = json_decode(wp_remote_retrieve_body(wp_remote_get($url)));
                         $bikeDistance = $bikeDistance->rows[0]->elements[0]->distance->value;
-                        set_transient('bicycle' . $customerCode . $shopCode, $bikeDistance);
+                        set_transient('bicycle' . $this->customerZipCode . $this->shopZipCode, $bikeDistance);
                     }
 
                     // Make distance into km.
@@ -94,7 +143,7 @@ function improvedShippingInit() {
                     $this->add_rate(array(
                         'id' => $this->id . 'bike',
                         'label' => 'Bike shipping',
-                        'cost' => $this->getBikeCost($totalWeight),
+                        'cost' => $this->getBikeCost($this->weight),
                         'calc_tax' => 'per_item'
                     ));
                 }
